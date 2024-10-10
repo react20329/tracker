9c0fd6093c8a01bd08245e4e557ce27d9907d4c4
anaumkhan <anaumkhan10@gmail.com>
2024-10-10 06:09:47 -0400

    Routes

---
+++ frontend/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Landing Page!</h1>
+      <p>This is the first page of the application.</p>
+    </div>
+  );
+}
